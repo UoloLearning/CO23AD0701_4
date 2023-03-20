5a1818f700bbdefe3bf6902dc42103efe769b126
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-20 13:24:11 +0530

    Create CO23AD0701_4_A1.py

---
+++ CO23AD0701_4_A1.py
@@ -0,0 +1,13 @@
+funnyLine = '''A cop pulled me
+over and asked "Papers",
+so I said
+"Scissors, I win!"
+and drove off.'''
+
+line_1 = "A cop pulled me\n"
+line_2 = "over and asked \"Papers\",\n"
+line_3 = "so I said\n"
+line_4 = "\"Scissors, I win!\"\n"
+line_5 = "and drove off."
+
+print(line_1,line_2,line_3,line_4,line_5)
